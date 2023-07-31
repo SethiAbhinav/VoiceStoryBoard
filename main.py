35d0aefc06858a86cce2f6851a9e94a2b41d45a5
Abhinav Sethi <abhinavabhinav33@gmail.com>
2023-08-01 00:07:54 +0530

    Logging, removed delete files logic

--- main.py
+++ main.py
@@ -153,8 +153,10 @@ def main():
             if audio_bytes:
                 with open('voices/user_response.wav', 'wb') as audio_file:
                     audio_file.write(audio_bytes)
-
+                print(" LOG : User response recorded ")
+                
             if os.path.exists('./voices/user_response.wav'):
+                print(" LOG : User response recorded and exists ")
                 user_response_text = whisper_asr()
                 ai_response = generate_ai_response(user_response_text, openai_api_key)
                 response_audio_path = './voices/ai_response.wav'
@@ -167,7 +169,7 @@ def main():
                 with st.expander('Closing Remarks'):
                     st.audio(response_audio_bytes, format='audio/wav')
                     st.write(ai_response)
-                delete_files("voices/")
+                # delete_files("./voices/")
         except Exception as e:
             print(f'Error: {e}')
             pass
