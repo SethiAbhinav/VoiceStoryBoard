f15de1b180b040eca09b3a5682cc667bbb28f535
Abhinav Sethi <abhinavabhinav33@gmail.com>
2023-08-01 00:59:21 +0530

    caching model, streamlit app was crashing

--- asr.py
+++ asr.py
@@ -1,8 +1,15 @@
 import whisper
 import speech_recognition as sr
+import streamlit as st
+
+@st.cache_resource()
+def load_whisper():
+    whisper_small = whisper.load_model("small")
+    return whisper_small
+
 
 def whisper_asr():
-    model = whisper.load_model("small")
+    model = load_whisper()
     result = model.transcribe('./voices/user_response.wav', fp16=False)
     transcribed_user_response = result['text']
     return transcribed_user_response
